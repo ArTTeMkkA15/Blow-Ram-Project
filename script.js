2321d57c89c518322f07107a251af302b18cf1fd
ILikeBeez <145926142+ILikeBeez@users.noreply.github.com>
2025-04-14 09:39:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function createExit() {
     let exit = document.querySelector(".exit");
+
+    let maxL = document.createElement("div");
+    maxL.className = "maxLeft";
+    let mLH = document.createElement("h1");
+    mLH.className = "mLH";
+    mLH.innerHTML = "&#11244;";
+    maxL.appendChild(mLH);
+
+    let arrowL = document.createElement("div");
+    arrowL.className = "leftArrow";
+    let arLH = document.createElement("h1");
+    arLH.className = "aLH";
+    arLH.innerHTML = "&#11207;";
+    arrowL.appendChild(arLH);
+
+    let arrowR = document.createElement("div");
+    arrowR.className = "rightArrow";
+    let arRH = document.createElement("h1");
+    arRH.className = "aRH";
+    arRH.innerHTML = "&#11208;";
+    arrowR.appendChild(arRH);
+
+    exit.appendChild(maxL);
+    exit.appendChild(arrowL);
+
+    let boxes = document.createElement("div");
+    boxes.className = "boxes";
     for(let i = 1; i <= 99; i++) {
         let box = document.createElement("div");
         box.className = "box";
@@ -13,6 +40,7 @@ function createExit() {
         let headerBox = document.createElement("div");
         header.className = "header";
         headerBox.className = "headerBox";
+        
         input.type = "number";
         input.disabled = "disabled";
         header.innerHTML = i;
@@ -21,6 +49,9 @@ function createExit() {
         inputBox.appendChild(input);
         box.appendChild(inputBox);
         box.appendChild(headerBox);
-        exit.appendChild(box);
+        boxes.appendChild(box);
     }
+    exit.appendChild(boxes);
+
+    exit.appendChild(arrowR);
 }
