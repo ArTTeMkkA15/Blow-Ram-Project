332082174901ed0353fa2b0d88528e81194221a1
ILikeBeez <145926142+ILikeBeez@users.noreply.github.com>
2025-05-20 12:06:31 +0200

    Add files via upload

--- Scripts/procesor.js
+++ Scripts/procesor.js
@@ -1,7 +1,3 @@
-const START_BUTTON = document.getElementById("startButton");
-const STOP_BUTTON = document.getElementById("stopButton");
-const PAUSE_BUTTON = document.getElementById("pauseButton");
-
 class Instruction {
     constructor(line, label, instruction, argument){
         this.line = line;
@@ -12,21 +8,3 @@ class Instruction {
 }
 
 let instructionArray = [];
-
-START_BUTTON.addEventListener("click", function getInstructions(){
-    for (i = 1; i <= instructionsAmount; i++){
-        let rowLable = document.getElementById(`label${i}`).innerText;    
-        let OptionsInstruction = document.getElementById(`instruction${i}`);
-        let rowInstruction = OptionsInstruction.options[OptionsInstruction.selectedIndex].text;
-        let rowArgument = document.getElementById(`argument${i}`).innerText;
-
-        if (rowLable == "" || rowArgument == "" || rowInstruction == ""){
-            alert("Fields: Lable, Instruciton and Argument cannot be empty!");
-            break;
-        } 
-        else {
-            const Code = new Instruction(i, rowLable, rowInstruction, rowArgument);
-            instructionArray[i] = Code;
-        }
-    }
-});
