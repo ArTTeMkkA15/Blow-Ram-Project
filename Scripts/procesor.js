69b98256501f6d0ec5fb7a4c398490d9cc9fba53
ArTTeMkkA <145377582+ArTTeMkkA15@users.noreply.github.com>
2025-05-26 19:36:08 +0200

    Fresh changes to Processor

--- Scripts/procesor.js
+++ Scripts/procesor.js
@@ -10,16 +10,11 @@ class Instruction {
 const START_BUTTON = document.getElementById("startButton");
 let instructionArray = [];
 let currentInputCell = 1;
+let acumulatorValue = null;
 let currentNum = 1;
-let currentNumJump = 1;
 let currentNumUpperIndex = 0;
-let jump = 0;
-let jgtz = 0;
-let jumpX = "";
-let jgtzX = "";
 let output = 1;
 
-let currentNumJgtz = 1;
 
 function getValue(argument) {
     if (argument.startsWith("=")) {
@@ -47,104 +42,129 @@ function setValue(argument, value) {
     }
 }
 
+function JUMP(instructionArray, startIndex, targetLabel) {
+    for (let i = startIndex; i < instructionArray.length; i++) {
+        console.log(instructionArray[i].label);
+        if (instructionArray[i].label == targetLabel) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 START_BUTTON.addEventListener("click", function getInstructions(){
     for (i = 1; i <= instructionsAmount; i++){
         let rowLable = document.getElementById(`label${i}`).innerText;    
         let OptionsInstruction = document.getElementById(`instruction${i}`);
         let rowInstruction = OptionsInstruction.options[OptionsInstruction.selectedIndex].text;
         let rowArgument = document.getElementById(`argument${i}`).innerText;
-
-        if (rowLable == "" || rowArgument == "" || rowInstruction == ""){
-            //alert("Fields: Lable, Instruciton and Argument cannot be empty!");
-            break;
-        } 
-        else {
-            const Code = new Instruction(i, rowLable, rowInstruction, rowArgument);
-            instructionArray[i] = Code;
-        }
+        const Code = new Instruction(i, rowLable, rowInstruction, rowArgument);
+        instructionArray[i] = Code;
     }
+
     console.log(instructionArray);
+
     while (currentNum <= instructionsAmount) {
-        let instructionRead = document.getElementById('instruction' + currentNum).value;
-        let argumentRead = document.getElementById('argument' + currentNum).textContent.trim();
-        let labelRead = document.getElementById('label' + currentNum).textContent.trim();
+        let isJump = false;
+        let currInstruction = instructionArray[currentNum].instruction;
+        let currArgument = instructionArray[currentNum].argument;
 
         let instructionInsert = document.getElementById('instruction');
         let argumentInsert = document.getElementById('argument');
 
-        if (instructionInsert) instructionInsert.value = instructionRead;
-        if (argumentInsert) argumentInsert.value = argumentRead;
+        if (instructionInsert) instructionInsert.value = currInstruction;
+        if (argumentInsert) argumentInsert.value = currArgument;
 
-        if (jump === 0) {
-            if (jgtz === 0) {
-                switch (instructionRead) {
-                    case "LOAD":
-                        setValue("0", getValue(argumentRead));
-                        break;
-                    case "STORE":
-                        setValue(argumentRead, getValue("0"));
-                        break;
-                    case "ADD":
-                        setValue("0", getValue("0") + getValue(argumentRead));
-                        break;
-                    case "SUB":
-                        setValue("0", getValue("0") - getValue(argumentRead));
-                        break;
-                    case "MULT":
-                        setValue("0", getValue("0") * getValue(argumentRead));
-                        break;
-                    case "DIV":
-                        setValue("0", Math.floor(getValue("0") / getValue(argumentRead)));
-                        break;
-                    case "READ":
-                        let inputValue = document.getElementById('input' + currentInputCell);
-                        if (inputValue) setValue(argumentRead, inputValue.value);
-                        currentInputCell++;
-                        break;
-                    case "WRITE":
-                        let outputValue = getValue(argumentRead);
-                        let outputX = document.getElementById('output' + output);
-                        if (outputX) outputX.value = outputValue;
-                        output++;
-                        break;
-                    case "JUMP":
-                        jumpX = labelRead;
-                        jump = 1;
-                        break;
-                    case "JGTZ":
-                        jgtzX = labelRead;
-                        jgtz = 1;
-                        break;
-                    case "HALT":
-                        currentNum = instructionsAmount + 1;
-                        break;
+        if(currInstruction == "JUMP" || currInstruction == "JGTZ" || currInstruction == "JZERO"){
+            console.log("FOUND JUMP TYPE COMAND");
+
+            if (currInstruction == "JUMP" && currArgument != ""){
+                console.log("FOUND SIMPLE JUMP COMAND");
+                let index = JUMP(instructionArray, currentNum, currArgument);
+                if (index !== -1) {
+                    console.log("JUMP COMAND COMPLEAT SUCCSSESFULLY");
+                    currentNum = index;
+                    isJump = true;
                 }
-            } else {
-                while (currentNumJump <= instructionsAmount) {
-                    let jgtzToCheck = document.getElementById('label' + currentNumJump).textContent.trim();
-                    if (jgtzToCheck === jgtzX && getValue("0") > 0) {
-                        jgtz = 0;
-                        currentNum = currentNumJump - 1;
-                        break;
+            }
+
+            if (currInstruction == "JGTZ" && currArgument != ""){
+                console.log("FOUND JGTZ COMAND");
+                if (acumulatorValue > 0){
+                    let index = JUMP(instructionArray, currentNum, currArgument);
+                    if (index !== -1) {
+                        console.log("JGTZ COMAND COMPLEAT SUCCSSESFULLY");
+                        currentNum = index;
+                        isJump = true;
                     }
-                    currentNumJump++;
                 }
-                currentNumJump = 1;
-            }
-        } else {
-            while (currentNumJump <= instructionsAmount) {
-                let jumpToCheck = document.getElementById('label' + currentNumJump).textContent.trim();
-                if (jumpToCheck === jumpX) {
-                    jump = 0;
-                    currentNum = currentNumJump - 1;
-                    break;
+                else {
+                    console.log("Acomulator value is not greater than 0, its value is: " + acumulatorValue);
                 }
-                currentNumJump++;
             }
-            currentNumJump = 1;
+
+            if (currInstruction == "JZERO" && currArgument != ""){
+                console.log("FOUND JZERO COMAND");
+                if (acumulatorValue == 0){
+                    let index = JUMP(instructionArray, currentNum, currArgument);
+                    if (index !== -1) {
+                        console.log("JZERO COMAND COMPLEAT SUCCSSESFULLY");
+                        currentNum = index;
+                        isJump = true;
+                    }
+                }
+                else {
+                    console.log("Acomulator value is not equal 0, its value is: " + acumulatorValue);
+                }
+            }
+
         }
 
-        currentNum++;
-        currentNumUpperIndex++;
-    }
+        if (!isJump){
+            switch (currInstruction) {
+                case "LOAD":
+                    setValue("0", getValue(currArgument));
+                    acumulatorValue = getValue(currArgument);
+                    break;
+                case "STORE":
+                    setValue(currArgument, getValue("0"));
+                    break;
+                case "ADD":
+                    setValue("0", getValue("0") + getValue(currArgument));
+                    acumulatorValue = getValue("0");
+                    break;
+                case "SUB":
+                    setValue("0", getValue("0") - getValue(currArgument));
+                    acumulatorValue = getValue("0");
+                    break;
+                case "MULT":
+                    setValue("0", getValue("0") * getValue(currArgument));
+                    acumulatorValue = getValue("0");
+                    break;
+                case "DIV":
+                    setValue("0", Math.floor(getValue("0") / getValue(currArgument)));
+                    acumulatorValue = getValue("0");
+                    break;
+                case "READ":
+                    let inputValue = document.getElementById('input' + currentInputCell);
+                    if (inputValue) setValue(currArgument, inputValue.value);
+                    currentInputCell++;
+                    break;
+                case "WRITE":
+                    let outputValue = getValue(currArgument);
+                    let outputX = document.getElementById('output' + output);
+                    if (outputX) outputX.value = outputValue;
+                    output++;
+                    break;
+                case "HALT":
+                    currentNum = instructionsAmount + 1;
+                    break;
+            }
+            currentNum++;
+            currentNumUpperIndex++;
+        }
+        else {
+            continue;
+        }
+    } 
 });
